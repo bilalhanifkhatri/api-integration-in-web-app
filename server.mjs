9cdb01e9fb59a020a27da0a638a010090e8e3651
bilalhanifkhatri <bilalhanifkhatri@gmail.com>
2023-01-01 07:57:15 +0500

    api completed

--- server.mjs
+++ server.mjs
@@ -16,7 +16,7 @@ let productSchema = new mongoose.Schema({
 });
 const productModel = mongoose.model("products", productSchema);
 
-app.post("/product", () => {
+app.post("/product", (req, res) => {
   const body = req.body;
   if (!body.name || !body.price || !body.catagory || !body.description) {
     res.status(400).send(` required parameter missing. example request body: 
@@ -28,13 +28,130 @@ app.post("/product", () => {
     }`);
     return;
   } else {
-    let newProduct = productModel.create({
-      name: body.name,
-      price: body.price,
-      catagory: body.catagory,
-      description: body.description,
-    });
+    let newProduct = productModel.create(
+      {
+        name: body.name,
+        price: body.price,
+        catagory: body.catagory,
+        description: body.description,
+      },
+      (err, saved) => {
+        if (!err) {
+          console.log(saved);
+          res.send({ meassage: "your product is saved" });
+        } else {
+          res.status(500).send({ meassage: "server error" });
+        }
+      }
+    );
   }
+});
+
+app.get("/products", (req, res) => {
+  productModel.find({}, (err, data) => {
+    // it will give you arrray of collections
+    if (!err) {
+      res.send({
+        meassage: "Here is your products list",
+        data: data,
+      });
+    } else {
+      res.status(500).send({
+        message: "server error",
+      });
+    }
+  });
+});
+app.get("/product/:id", (req, res) => {
+  const id = req.params.id;
+  productModel.findOne({ _id: id }, (err, data) => {
+    // it will only one collection.
+    if (!err) {
+      if (data) {
+        res.send({
+          meassage: "Here is your product",
+          data: data,
+        });
+      } else {
+        res.status(404).send({
+          meassage: "product not found",
+        });
+      }
+    } else {
+      res.status(500).send({
+        message: "server error",
+      });
+    }
+  });
+});
+
+app.put("/product/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+
+    if (!body.name || !body.price || !body.catagory || !body.description) {
+      res.status(400).send(` required parameter missing. example request body: 
+      {
+          "name": "value"
+          "price": 1234
+          "catagory": "value"
+          "description": "value" 
+      }`);
+      return;
+    } else {
+      const data = await productModel
+        .findByIdAndUpdate(
+          id,
+          {
+            name: body.name,
+            price: body.price,
+            catagory: body.catagory,
+            description: body.description,
+          },
+          { new: true }
+        )
+        .exec();
+      console.log("Updated: ", data);
+      res.send({
+        message: "product is updated successfully",
+        data: data,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({ message: "server error" });
+  }
+});
+
+app.delete("products", (req, res) => {
+  productModel.deleteMany({}, (err, data) => {
+    if (!err) {
+      res.send({
+        message: "All Products has been deleted successfully",
+      });
+    } else {
+      res.status(500).send({ message: "server error" });
+    }
+  });
+});
+app.delete("product/:id", (req, res) => {
+  const id = req.params.id;
+  productModel.deleteOne({ _id: id }, (err, deletedData) => {
+    console.log("Deleted Data: ", deletedData);
+    if (!err) {
+      if (deletedData.deletedCount !== 0) {
+        res.send({
+          message: "Product has been deleted successfully",
+        });
+      } else {
+        res.send({
+          message: "No Product found with this id:" + id,
+        });
+      }
+    } else {
+      res.status(500).send({ message: "server error" });
+    }
+  });
 });
 
 app.get("/weather", (req, res) => {
